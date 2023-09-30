9f5c5d38d395cbeb8d75a967bb0b30820a1b6922
HassanToyin <143343798+HassanToyin@users.noreply.github.com>
2023-09-30 19:34:12 +0100

    Add files via upload

---
+++ forms.js
@@ -0,0 +1,43 @@
+function add(a,b){
+    console.log(a+b)
+}
+add = (3,5);
+add = (4,5)
+
+//let name ="toyin"
+function greet(name){
+    console.log("hello" + name)
+}
+
+greet ("toyin")
+
+function double(number){
+    console.log(2 * number)
+}
+double(20);
+
+//function checkPassword() {
+    //let pass = document.getElementById("pass")
+    //let confirm = document.getElementById("not pass")
+
+    //if (pass === noon){
+        //alert("Access granted")
+    //}else {
+        //alert("Acess denied")
+    //}
+//}
+
+//if (password)
+
+console.log(document)
+function passwordVerify(){
+    let noon = document.getElementById('noon').Value;
+    let noon2 = document.getElementById('noon1').Value;
+
+    if(noon === noon2){
+        alert("password match")
+    }else(
+        alert("password do not match")
+    )
+}
+
